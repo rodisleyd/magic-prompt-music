7c0e5901b1708d02345cfd0c01787850dcabc054
rodisleyd <rodisleijose@gmail.com>
2026-03-16 14:36:41 -0300

    fix: add missing imports to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,8 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import { VitePWA } from 'vite-plugin-pwa';
+import path from 'path';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
